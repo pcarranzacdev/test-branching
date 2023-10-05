1796642c7a82065a799e2876afad01ed825cdea6
pcarranzac <pierina.carranza@rimac.com.pe>
2023-10-05 00:03:53 -0500

    función multiplicacon

--- sumar.js
+++ sumar.js
@@ -2,3 +2,6 @@ const Math = require('./src/helpers/operacionesMatematicas');
 
 const sum = Math.sum([4, 3, 5]);
 console.log(`El resultado de la suma es ${sum}`);
+
+const multiplication = Math.multiplication([4, 1]);
+console.log(`El resultado de la multiplication es ${multiplication}`);
