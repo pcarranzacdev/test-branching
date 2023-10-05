18ca1fa0299986ca279edfee319ce68f713c03c2
pcarranzac <pierina.carranza@rimac.com.pe>
2023-10-05 00:01:31 -0500

    función resta

--- src/helpers/operacionesMatematicas.js
+++ src/helpers/operacionesMatematicas.js
@@ -2,6 +2,10 @@ class Math {
   static sum(aNumbers = []) {
     return aNumbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   }
+
+  static subtraction(a, b) {
+    return a - b;
+  }
 }
 
 module.exports = Math;
--- sumar.js
+++ sumar.js
@@ -2,3 +2,6 @@ const Math = require('./src/helpers/operacionesMatematicas');
 
 const sum = Math.sum([4, 3, 5]);
 console.log(`El resultado de la suma es ${sum}`);
+
+const subtraction = Math.subtraction(6, 9);
+console.log(`El resultado de la resta es ${subtraction}`);
