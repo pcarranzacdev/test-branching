8a1350dd836e52c7d2d29e8cc47a5a728da717f1
pcarranzac <pierina.carranza@rimac.com.pe>
2023-10-05 00:03:53 -0500

    función multiplicacon

--- src/helpers/operacionesMatematicas.js
+++ src/helpers/operacionesMatematicas.js
@@ -2,6 +2,10 @@ class Math {
   static sum(aNumbers = []) {
     return aNumbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   }
+
+  static multiplication(aNumbers = []) {
+    return aNumbers.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+  }
 }
 
 module.exports = Math;
